e5b8c7be0cf65d9819d1183fc52e5077d5b2528a
5mateosjuan <5mateosjuan@gmail.com>
2021-03-08 11:03:00 -0600

    Add MoreWhiteBoardP project

---
+++ MoreWhiteBoardP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MoreWhiteBoardP
+{
+    class Program
+    {
+        static Random rand = new Random(123);
+        static void Main(string[] args)
+        {
+
+            int number = 10;
+
+            int sum = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                sum += Results();
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+
+        static public int Results() 
+        {
+            int low = 1;
+            int high = 100;
+
+            return rand.Next(low, high + 1); 
+        }
+        
+    }
+}
